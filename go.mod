3850faf9cd1229cdec09cf81c09e046141c1bfd8
Rooney Lee <rooney.lee@themedium.io>
2021-06-18 16:30:40 +0900

    add initial files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rooney-lee/besu-cli
+
+go 1.14
+
+require github.com/spf13/cobra v1.1.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rooney-lee/besu-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
