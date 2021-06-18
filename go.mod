77d84c4b000a9af72cd1bf6a4491961060c9f8ea
Rooney Lee <rooney.lee@themedium.io>
2021-06-18 18:14:27 +0900

    add models

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/rooney-lee/besu-cli
 
 go 1.14
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/ethereum/go-ethereum v1.10.4 // indirect
+	github.com/spf13/cobra v1.1.3
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+)
---
+++ model/model.go
@@ -0,0 +1,48 @@
+package model
+
+// Block data structure
+type Block struct {
+	BlockNumber       int64         `json:"block_number"`
+	Timestamp         uint64        `json:"timestamp"`
+	Difficulty        uint64        `json:"difficulty"`
+	Hash              string        `json:"hash"`
+	TransactionsCount int           `json:"transactions_count"`
+	Transactions      []Transaction `json:"transactions"`
+}
+
+// Transaction data structure
+type Transaction struct {
+	Hash     string `json:"hash"`
+	Value    string `json:"value"`
+	Gas      uint64 `json:"gas"`
+	GasPrice uint64 `json:"gas_price"`
+	Nonce    uint64 `json:"nonce"`
+	To       string `json:"to"`
+	Pending  bool   `json:"pending"`
+}
+
+// TransferEthRequest data structure
+type TransferEthRequest struct {
+	PrivateKey string `json:"private_key"`
+	To      string `json:"to"`
+	Amount  int64  `json:"amount"`
+}
+
+// HashResponse data structure
+type HashResponse struct {
+	Hash string `json:"hash"`
+}
+
+// BalanceResponse data structure
+type BalanceResponse struct {
+	Address string `json:"address"`
+	Balance string `json:"balance"`
+	Symbol  string `json:"symbol"`
+	Units   string `json:"units"`
+}
+
+// Error data structure
+type Error struct {
+	Code    uint64 `json:"code"`
+	Message string `json:"message"`
+}
